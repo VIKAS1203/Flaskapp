ceeb3484dc8241f187b05fbc96093b1d3268ed29
VJShetty <44110098+VIKAS1203@users.noreply.github.com>
2024-07-24 12:11:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 data "aws_ami" "latest_amazon_linux" {
   owners      = ["amazon"]
   most_recent = true
+
   filter = {
     name = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
